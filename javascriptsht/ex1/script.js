02c2aa2020c4827928014c3a62434cac6546389d
Daniel Oliveira <daniel11oliveirasunzin@gmail.com>
2024-11-17 21:28:01 -0300

    telas menores projeto completo

--- javascriptsht/ex1/script.js
+++ javascriptsht/ex1/script.js
@@ -3,22 +3,24 @@ function carregar(){
 var agora = new Date()
 var horario = agora.getHours()
 var min = agora.getMinutes()
-var seg = agora.getSeconds()
-var mili = agora.getMilliseconds()
 var resp = document.getElementById('res')
 var hora = document.getElementById('bom')
 var corpo = document.body
+var img  = document.getElementById('img')
 
-resp.innerHTML = `Agora são ${horario}:${min}:${seg}:${mili}`
+resp.innerHTML = `Agora são ${horario}:${min}`
 
 if(horario > 5 && horario < 12){
     hora.innerHTML = 'Bom dia'
     corpo.style.backgroundColor = 'lightblue'
+    img.src = 'imagens/dia.jpg'
 }else if(horario > 11 && horario < 18){
     hora.innerHTML = 'Boa tarde'
     corpo.style.backgroundColor = 'Orange'
+    img.src = 'imagens/noite.jpg'
 }else if(horario > 17 && horario < 24){
     hora.innerHTML = 'Boa noite'
     corpo.style.backgroundColor = 'gray'
+    img.src = 'imagens/noiteofc.jpg'
 }
 }
